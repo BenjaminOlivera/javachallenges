f5b59d6b502951fd24fc18810f409620a9d503fc
Brian Benjamin Olivera Colonio <unknowsoldierbj@gmail.com>
2023-03-29 15:52:32 -0500

    math method

--- PracticeJava/src/vista1/Numbersmat.java
+++ PracticeJava/src/vista1/Numbersmat.java
@@ -4,7 +4,16 @@ public class Numbersmat {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+      int myInt = 5;
+      double myDouble = 4;
+    /*  System.out.print(myInt/myDouble);*/
+     /* System.out.print(10 + myInt);*/
+      int ageb = 1992;
+      int currentyear = 2023;
+      int myNegative = -199;
+     /* System.out.print(Math.abs(myNegative)); /*abs te da el valor absoluto de una cantidad*/ 
+     /* System.out.print(Math.pow(ageb , 2)); /*da la potencia*/
+      System.out.print(Math.sqrt(121));  /*da el cuadrado para obtener dicha potencia*/
 	}
 
 }

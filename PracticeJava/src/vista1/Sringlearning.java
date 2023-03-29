510b8137eb14f44133c0018a9193338c8cd94d49
Brian Benjamin Olivera Colonio <unknowsoldierbj@gmail.com>
2023-03-29 13:14:27 -0500

    string exercises

--- PracticeJava/src/vista1/Sringlearning.java
+++ PracticeJava/src/vista1/Sringlearning.java
@@ -4,7 +4,16 @@ public class Sringlearning {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+     System.out.println("Benjamin \n Olivera"); /*backslash para salto de linea*/
+     
+     String phrase = "brian benjamin olivera colonio";
+     System.out.println(phrase.toUpperCase());  /* la variable se imprie sin comillas*/ /*usando el metodo toUpperCase*/
+     System.out.println(phrase.toLowerCase());
+     System.out.println(phrase.length()); /*numero de caracteres*/
+     System.out.println(phrase.contains("benjamin"));
+     System.out.println(phrase.charAt(3)); /* me da la letra que ocupa el tercer lugar*/
+     System.out.println(phrase.indexOf("b")); /* me da el primer caracter del string*/
+     System.out.println(phrase.lastIndexOf("b"));
 	}
 
 }

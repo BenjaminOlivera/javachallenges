18edd7a357e13aa4e9c03215465141c54026cb3a
Brian Benjamin Olivera Colonio <unknowsoldierbj@gmail.com>
2023-03-29 12:12:48 -0500

    strings

---
+++ PracticeJava/src/vista1/Print.java
@@ -0,0 +1,18 @@
+package vista1;
+
+public class Print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	 String nameChar = "Benjamin";
+	 int ageChar = 30;
+	 String fanOff = "watercolours";
+		
+     System.out.println("The was a man called "+nameChar+",");
+     System.out.println("he was "+ ageChar+" years old");
+     System.out.println("he also was a big fan of "+fanOff+",");
+     System.out.println( nameChar+" also enjoys to read and draw");
+	}
+
+}
---
+++ PracticeJava/src/vista1/Sringlearning.java
@@ -0,0 +1,10 @@
+package vista1;
+
+public class Sringlearning {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

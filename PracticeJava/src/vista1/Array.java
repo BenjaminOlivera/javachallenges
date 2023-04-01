264f9953d9885f331b405b9734f3326eb230e069
Brian Benjamin Olivera Colonio <unknowsoldierbj@gmail.com>
2023-04-01 18:26:46 -0500

    array

---
+++ PracticeJava/src/vista1/Array.java
@@ -0,0 +1,20 @@
+package vista1;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     String [] friends = {"Ben","Ryan", "Stephen"};
+     friends[0] = "Benoni";
+     System.out.println(friends);
+     System.out.println(friends[0]);
+     System.out.println(friends.length);
+     
+     String [] friendsList = new String[3];
+     friendsList[0] = "Benoni";
+     friendsList[1] = "Mark";
+     friendsList[2] = "Jhon";
+     System.out.println(friendList[]);
+     }
+
+}

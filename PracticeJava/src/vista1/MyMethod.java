b4c4e2e64d6a218ea710a37e6c5c0ecd1757d751
Brian Benjamin Olivera Colonio <unknowsoldierbj@gmail.com>
2023-04-03 22:35:16 -0500

    Method review

---
+++ PracticeJava/src/vista1/MyMethod.java
@@ -0,0 +1,20 @@
+package vista1;
+
+public class MyMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         
+         System.out.println("top");
+         sayHello();
+         System.out.println("bottom");
+	}
+	 public static void sayHello() {
+		
+		String name = "Benjamin";
+        int age = 30;		
+		System.out.println("Hello "+ name + " you are " + age + " years old" );
+		 
+	 }
+	
+}

b85c95e3c8728f97a45d4a99698b2b37b4c43eee
Brian Benjamin Olivera Colonio <unknowsoldierbj@gmail.com>
2023-04-04 23:17:25 -0500

    return basics

---
+++ PracticeJava/src/vista1/Returnstatement.java
@@ -0,0 +1,14 @@
+package vista1;
+
+public class Returnstatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println(cube(3));
+	}
+	public static int cube(int numberToCube) {
+		return (numberToCube*numberToCube*numberToCube);
+	}
+	
+
+}
